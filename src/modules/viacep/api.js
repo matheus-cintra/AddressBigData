4e978c1b089cb755506d93c4db88c98bc3b312af
Matheus <matheus_cintra@hotmail.com>
2020-08-03 19:59:53 -0300

    added force param to route

--- src/modules/viacep/api.js
+++ src/modules/viacep/api.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import Address from './model';
-import axios from 'axios';
+import express from "express";
+import Address from "./model";
+import axios from "axios";
 
 const router = express.Router();
-const viaCepUri = 'https://viacep.com.br/ws/';
+const viaCepUri = "https://viacep.com.br/ws/";
 
-router.get('/api/v1/addressBigData/:cep', async (req, res) => {
+router.get("/api/v1/addressBigData/:cep/:force?", async (req, res) => {
   try {
-    let { cep } = req.params;
+    let { cep, force } = req.params;
 
     if (!cep)
       return res
         .status(404)
-        .json({ sucess: false, data: { message: 'CEP not provided' } });
+        .json({ sucess: false, data: { message: "CEP not provided" } });
 
-    cep = cep.replace(/[^0-9]/g, '');
+    cep = cep.replace(/[^0-9]/g, "");
 
     if (cep.length !== 8)
       return res.status(404).json({
         sucess: false,
-        data: { message: 'CEP must contain 8 digits' }
+        data: { message: "CEP must contain 8 digits" },
       });
 
     const cepExists = await Address.findOne({ cep: cep });
 
-    if (cepExists) return res.status(200).json(cepExists);
+    if (cepExists && !force) return res.status(200).json(cepExists);
 
     let newCep = await axios.get(`${viaCepUri}${cep}/json/`);
 
-    if(newCep.data.erro) {
+    if (newCep.data.erro) {
       return res.status(404).json({
         sucess: false,
         data: {
-          message: 'Error in get cep'
-        }
-      })
+          message: "Error in get cep",
+        },
+      });
     }
-    
+
     newCep = newCep.data;
 
     const address = {
-      cep: newCep.cep.replace(/[^0-9]/g, ''),
+      cep: newCep.cep.replace(/[^0-9]/g, ""),
       address: newCep.logradouro,
       additional: newCep.complemento,
       neighborhood: newCep.bairro,
@@ -48,20 +48,25 @@ router.get('/api/v1/addressBigData/:cep', async (req, res) => {
       state: newCep.uf,
       unity: newCep.unidade,
       ibge: newCep.ibge,
-      gia: newCep.gia
+      gia: newCep.gia,
     };
 
-    const result = await Address.create({ ...address });
+    const result = cepExists
+      ? await Address.create({ ...address })
+      : await Address.updateOne(
+          { _id: cepExists._id },
+          { $set: { ...address } }
+        );
 
     if (!result)
       return res
         .status(500)
-        .json({ sucess: false, data: { message: 'Error in bigData' } });
+        .json({ sucess: false, data: { message: "Error in bigData" } });
 
     return res.status(200).json(address);
   } catch (error) {
-    console.warn('Erro bigdata > ', error);
+    console.warn("Erro bigdata > ", error);
   }
 });
 
-module.exports = app => app.use(router);
+module.exports = (app) => app.use(router);
